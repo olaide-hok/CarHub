c7c102a94d135cf37e22183b5f73c5b7b3c9bede
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-28 22:10:28 +0100

    Complete carCard LWC

--- force-app/main/default/lwc/carCard/carCard.js
+++ force-app/main/default/lwc/carCard/carCard.js
@@ -1,7 +1,10 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+
+//Navigation
+import { NavigationMixin } from "lightning/navigation";
 
 //Car__c Schema
-
+import CAR_OBJECT from "@salesforce/schema/Car__c";
 import NAME_FIELD from "@salesforce/schema/Car__c.Name";
 import PICTURE_URL_FIELD from "@salesforce/schema/Car__c.Picture_URL__c";
 import CATEGORY_FIELD from "@salesforce/schema/Car__c.Category__c";
@@ -12,7 +15,20 @@ import SEATS_FIELD from "@salesforce/schema/Car__c.Number_of_Seats__c";
 import CONTROL_FIELD from "@salesforce/schema/Car__c.Control__c";
 // getFieldValue function is used to extract field values
 import { getFieldValue } from "lightning/uiRecordApi";
-export default class CarCard extends LightningElement {
+
+//lightning message service
+import {
+  subscribe,
+  MessageContext,
+  unsubscribe
+} from "lightning/messageService";
+import CAR_SELECTED_MESSAGE from "@salesforce/messageChannel/CarSelected__c";
+
+export default class CarCard extends NavigationMixin(LightningElement) {
+  /// load content for LMS
+  @wire(MessageContext)
+  messageContext;
+
   //exposing fields to make them available in the template
   categoryField = CATEGORY_FIELD;
   makeField = MAKE_FIELD;
@@ -22,15 +38,51 @@ export default class CarCard extends LightningElement {
   controlField = CONTROL_FIELD;
 
   //Id of Car__c to display data
-  recordId = "a002w00000F40nPAAR";
+  recordId;
 
   // car fields displayed with specific format
   carName;
   carPictureUrl;
+
+  //subscription reference for carSelected
+  carSelectionSubscription;
+
   handleRecordLoaded(event) {
     const { records } = event.detail;
     const recordData = records[this.recordId];
     this.carName = getFieldValue(recordData, NAME_FIELD);
     this.carPictureUrl = getFieldValue(recordData, PICTURE_URL_FIELD);
+  }
+
+  connectedCallback() {
+    this.subscribeHandler();
+  }
+
+  subscribeHandler() {
+    this.carSelectionSubscription = subscribe(
+      this.messageContext,
+      CAR_SELECTED_MESSAGE,
+      (message) => this.handleCarSelected(message)
+    );
+  }
+  handleCarSelected(message) {
+    this.recordId = message.carId;
+  }
+
+  disconnectedCallback() {
+    unsubscribe(this.carSelectionSubscription);
+    this.carSelectionSubscription = null;
+  }
+
+  /**navigate to record page */
+  handleNavigateToRecord() {
+    this[NavigationMixin.Navigate]({
+      type: "standard__recordPage",
+      attributes: {
+        recordId: this.recordId,
+        objectApiName: CAR_OBJECT.objectApiName,
+        actionName: "view"
+      }
+    });
   }
 }
